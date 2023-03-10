e6f6e85230a7f349a40a0f22546cf1e9b149cd6b
Adam-Barto <adam.jbarto@gmail.com>
2023-03-10 14:13:36 -0500

    Get dog ID and corrected Remove tests

--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,11 +1,14 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.w3c.dom.ls.LSOutput;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
 
-import java.util.Date;
+import java.sql.Date;
+import java.sql.SQLOutput;
 
 /**
  * @author leon on 4/19/18.
@@ -17,18 +20,77 @@ public class DogHouseTest {
     // TODO - Create tests for `Dog getDogById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfDogs()`
 
+//    @Test
+//    public void testGetNumberOfDogs() {
+//        // Given (some)
+//        int expected = 1;
+//        String name = "Milo";
+//        Date birthDate = Date.valueOf("2018-10-03");
+//        Dog animal = AnimalFactory.createDog(name, birthDate);
+//        DogHouse.clear();
+//
+//        // When
+//        DogHouse.add(animal);
+//
+//        // Then
+//        DogHouse.getNumberOfDogs();
+//    }
+
     @Test
-    public void testGetNumberOfDogs() {
-        // Given (some
-        String name = "Milo";
-        Date birthDate = new Date();
-        Dog animal = AnimalFactory.createDog(name, birthDate);
+    public void testAddDog() {
+        String name = "Wren";
+        Date date = Date.valueOf("2018-10-03");
+        Dog expected = AnimalFactory.createDog(name, date);
+
+        DogHouse.add(expected);
+
+        Dog actual = DogHouse.getDogById(0);
+
+        Assert.assertEquals(expected, actual);
         DogHouse.clear();
-
-        // When
-        DogHouse.add(animal);
-
-        // Then
-        DogHouse.getNumberOfDogs();
     }
+    @Test
+    public void testRemoveDogId() {
+        String name = "Wren";
+        Date date = Date.valueOf("2018-10-03");
+        Dog animal = AnimalFactory.createDog(name, date);
+        DogHouse.add(animal);
+        Dog expected = null;
+
+        DogHouse.remove(0);
+
+        Dog actual = DogHouse.getDogById(0);
+
+        Assert.assertEquals(expected, actual);
+        DogHouse.clear();
+    }
+    @Test
+    public void testRemoveDog() {
+        String name = "Wren";
+        Date date = Date.valueOf("2018-10-03");
+        Dog animal = AnimalFactory.createDog(name, date);
+        DogHouse.add(animal);
+        Dog expected = null;
+
+        DogHouse.remove(animal);
+
+        Dog actual = DogHouse.getDogById(0);
+
+        Assert.assertEquals(expected, actual);
+        DogHouse.clear();
+    }
+    @Test
+    public void getDogById() {
+        String name = "Wren";
+        Date date = Date.valueOf("2018-10-03");
+        Dog animal = AnimalFactory.createDog(name, date);
+        Dog expected = animal;
+
+        DogHouse.add(animal);
+        Dog actual = DogHouse.getDogById(0);
+
+        Assert.assertEquals(expected, actual);
+        DogHouse.clear();
+    }
+
 }
